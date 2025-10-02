48c1b51140090383c39c05dca045ac31c880326e
ayman mady <ayman.m.mady@gmail.com>
2025-10-02 12:20:19 +0300

    restructure the project

---
+++ src/main.cpp
@@ -0,0 +1,57 @@
+#include <cstdio>
+
+// #include "../include/datastructure/nodes.h"
+// #include "../include/datastructure/queue.h"
+// #include "../include/datastructure/stack.h"
+#include "../include/sort/insertion_sort.h"
+
+int main() {
+    // __line_node_with_one_direction__<double> single(1);
+    // __line_node_with_two_direction__<double> dble(50);
+    // single.set_val(10.5);
+    // single.set_next(new __line_node_with_one_direction__<double>(2.88));
+    // dble.set_next(new __line_node_with_two_direction__<double>(60.74588));
+    // dble.set_prev(new __line_node_with_two_direction__<double>(280.88));
+
+    // printf(" single info : val : %f , next_val : %f  \n|| double info : val : %f , next : %f , prev : %f"
+    //            , single.get_val() , single.next()->get_val() , dble.get_val() , dble.next()->get_val() , dble.prev()->get_val() );
+
+    // printf(" \n \n  %d \n\n" , ( dble==dble ));
+
+    // containers::stack<double> q ;
+
+
+    // q.push(50.501);
+    // q.push(60.89);
+    // q.push(8258.2588);
+    // q.push(89522);
+    // q.emplace(100);
+    // printf("q size : %llu \n q top : %f \n" , q.size() , q.top() );
+    //  q.pop(); q.pop(); q.pop(); q.pop(); q.pop();
+
+    //  printf("size : %f" , q.top());
+
+    // auto x = 99;
+    // auto& y = x;
+
+    // y++;
+    // printf("%d" , x);
+    double arry1 []= { 1 , 2 , 8 , 9 , -78 , 0 ,14 , 8 , 3 , 5};
+    std::array<double , 10> arr =  { 1 , 2 , 8 , 9 , -78 , 0 ,14 , 8 , 3 , 5};
+    sort::insertion_sort::c_style_array_ref_sort(arry1);
+    sort::insertion_sort::stl_array_ref_sort<double , 10>(arr);
+
+    printf("original arr = { 1 , 2 , 8 , 9 , -78 , 0 ,14 , 8 , 3 , 5} \n");
+
+    printf("ordar array1 = { ");
+    for ( unsigned long long i =0 ; i < 10 ; i++){
+        printf(" %f , " , arry1[i]);
+    }
+    printf("}  \n\n");
+    printf("ordar arr = { ");
+    for ( unsigned long long i =0 ; i < 10 ; i++){
+        printf(" %f , " , arr[i]);
+    }
+    printf("}  \n\n");
+
+}
